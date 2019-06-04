587d3911610efc11f377bcbe5479acc00c82ef31
zbyzby11 <45311865+zbyzby11@users.noreply.github.com>
2019-06-04 21:43:30 +0800

    update

--- src/predicate.py
+++ src/predicate.py
@@ -1,2 +1,8 @@
-def predicate_exisit(appid):
-    ...
+import codecs
+import json
+
+DATA_INPUT_DIR = json.load(codecs.open('../config/config.json'))["data_input_dir"]
+
+def predicate_exisit(appname):
+    app2id_dict = json.load(open(DATA_INPUT_DIR + 'app2id_dict.txt', 'r', encoding='utf8'))
+    entity2id_dict = json.load(open(DATA_INPUT_DIR + 'entity2id_dict.txt', 'r', encoding='utf8'))
